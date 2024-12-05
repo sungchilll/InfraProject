c5cf1fcbf4a3bfb54484911aab373c2c60df1999
loophyun <dhkim960511@naver.com>
2024-12-05 11:49:34 +0900

    add feature: searchAll, searchByNo - EmpAPIController

---
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -0,0 +1,38 @@
+package app.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.Emp;
+import app.entity.ExceptionMessage;
+import app.repository.EmpRepository;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/emps")
+public class EmpAPIController {
+
+	private final EmpRepository empRepository;
+	
+	@GetMapping
+	ResponseEntity<?> searchAll(){
+		List<Emp> emps = empRepository.findAll();
+		if(emps.isEmpty()) return new ResponseEntity<>(new ExceptionMessage("사원정보가 존재하지 않습니다"), HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(emps, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{empno}")
+	ResponseEntity<?> SearchByNo(@PathVariable Integer empno){
+		Optional<Emp> emp = empRepository.findById(empno);
+		if(!emp.isPresent()) return new ResponseEntity<>(new ExceptionMessage("사원정보가 존재하지 않습니다"), HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(emp, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/app/entity/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionMessage {
+	private String msg;
+}
